9b5bddb77af3fcfda6fae34023071bc806a68a7a
Abhishek Maheshwari <abhishek27082000@gmail.com>
2025-02-09 15:16:47 +0530

    Enhancement: Improved video player positioning

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,14 +1,16 @@
 import React from "react";
+import "./VideoPlayer.css";
 
 function VideoPlayer() {
   return (
-    <React.Fragment>
-      <div className="video-container">
-        <video controls autoPlay>
-          <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4"></source>
-        </video>
-      </div>
-    </React.Fragment>
+    <div className="video-container">
+      <video id="video" className="video" controls autoPlay>
+        <source
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          type="video/mp4"
+        ></source>
+      </video>
+    </div>
   );
 }
 
